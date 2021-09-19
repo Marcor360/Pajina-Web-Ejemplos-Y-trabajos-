a3e1e131439e954c77c29a85cc5d8a3c1e6fb573
Marco Antonio Rulfo Castro <81320236+Marcor360@users.noreply.github.com>
2021-09-19 13:38:30 -0500

    Creacion de PHP
    Integracion de IndexPHP

---
+++ Index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet " type="text/css" href=CSS\bootstrap.rtl.min.css>
+    <title>Menu Con Bootstrap</title>
+  </head>
+  <body>
+    <h1>Hola</h1>
+  </body>
+</html>
