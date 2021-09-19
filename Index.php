6f20b866a9b8b26bf49de66db524089247740742
Marco Antonio Rulfo Castro <81320236+Marcor360@users.noreply.github.com>
2021-09-19 15:05:12 -0500

    Integracion de Nadvar
    Integracion de un Nabvar (Menu de navegacion) a la pajina web

--- Index.php
+++ Index.php
@@ -3,9 +3,59 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet " type="text/css" href=CSS\bootstrap.rtl.min.css>
-    <title>Menu Con Bootstrap</title>
+    <title>Inicion|MRJD</title>
   </head>
   <body>
-    <h1>Hola</h1>
+    <!--- Inicio de Menu --->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navega</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">¿Quienes somos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Proyectos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Reseñas y Opiniones</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Servicios
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Instalacion de Redes</a></li>
+            <li><a class="dropdown-item" href="#">Mantenimiento a equipos de computo</a></li>
+            <li><a class="dropdown-item" href="#">Gestion de BD</a></li>
+            <li><a class="dropdown-item" href="#">Desarollo de Pajinas Webs</a></li>
+            <li><a class="dropdown-item" href="#">Desarollo de Programas Informaticos</a></li>
+            <li><a class="dropdown-item" href="#">Cotizacion</a></li>
+            <li><hr class="dropdown-divider">Contactanoso</li>
+            <li><a class="dropdown-item" href="#">Habla con un operador</a></li>
+            <li><a class="dropdown-item" href="#">Mandanos un mensage </a></li>
+            <li><a class="dropdown-item" href="#">Por redes sociales</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Muy Pronto</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Busqueda</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    <!--- Fin de Menu --->
+    <script src="JS\bootstrap.min.js" ></script>
   </body>
 </html>
